2c2cefb3af4c7d10beaa919a9e38c7e1cff84606
KGr1sha <krylov.grigory@gmail.com>
2025-03-19 22:31:04 +0300

    duck duck search is so bad this is absolute disaster

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 import streamlit as st
-import asyncio
-import aiohttp
 import google.generativeai as genai
 from dotenv import load_dotenv
+from langchain_core.messages.human import HumanMessage
+from langchain_core.messages.ai import AIMessage
 import os
 
-from search_agent import search_courses
+from search_agent import search_courses_duck, get_course_info
 
 def init_session():
     st.session_state["initialized"] = True;
@@ -38,10 +38,10 @@ if "initialized" not in st.session_state:
     init_session();
 
 
-
+st.set_page_config(layout="wide")
 st.title('Search ur shit')
 
-col1, col2, col3 = st.columns([1, 1, 1], border=True)
+col1, col2, col3 = st.columns([1, 1, 1], border=False)
 
 with col1:
     platform = st.radio('Platform', ['Stepik', 'Coursera'])
@@ -53,9 +53,13 @@ with col3:
     search_courses = st.button('Search courses')
     search_info = st.button('Search info')
 
+if "answer" not in st.session_state:
+    st.session_state["answer"] = list()
 
 if search_courses and course_name and platform:
-    st.write(search_courses(course_name, platform))
+    response = search_courses_duck(course_name, platform)
+    st.write(response)
 elif search_info and course_name and platform:
-    st.write('nah')
+    response = get_course_info(course_name, platform)
+    st.write(response)
 
--- search_agent.py
+++ search_agent.py
@@ -7,37 +7,83 @@ from langchain_community.document_loaders import RecursiveUrlLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_experimental.tools.python.tool import PythonREPLTool
 from langgraph.prebuilt import create_react_agent
-from langchain.tools import DuckDuckGoSearchRun
-from langchain.embeddings import HuggingFaceEmbeddings
+from langchain_community.tools import DuckDuckGoSearchRun
+from langchain_community.embeddings import HuggingFaceEmbeddings
 from huggingface_hub import login
-from langchain.vectorstores import FAISS
+from langchain_community.vectorstores import FAISS
 
-def search_courses(course_name, platform):
+def search_courses_rag(course_name: str, platform_url: str) -> str:
     load_dotenv()
+
+    loader = RecursiveUrlLoader(platform_url)
+    docs = loader.load()
     llm = ChatGroq(model="llama3-70b-8192")
-    python_tool = PythonREPLTool()
+
+    text_contents = [doc.page_content for doc in docs]
+    splitter = RecursiveCharacterTextSplitter(
+        chunk_size=500,
+        chunk_overlap=100,
+        length_function=len,
+    )
+    split_documents = splitter.create_documents(text_contents)
 
     login(os.getenv('HUGGING_FACE_TOKEN'))
-
     emb_model = HuggingFaceEmbeddings(model_name="intfloat/multilingual-e5-small")
 
+    vector_store = FAISS.from_documents(split_documents, emb_model)
+    vector_store.save_local("vector_store")
+
+    retriever = vector_store.as_retriever(
+        search_type="similarity",
+        k=3,
+        score_threshold=None,
+    )
+    tool = create_retriever_tool(
+        retriever,
+        "https://stepik.org/catalog",
+        "Найди и верни курсы на stepik",
+    )
+    agent_chain = create_react_agent(llm, [tool])
+
+    query = f"Подбери мне онлайн-курсы по {course_name} с практическими заданиями."
+    result = agent_chain.invoke({
+        "messages": [{"role": "user", "content": query}]
+    })
+
+    return result["messages"][-1].content
+
+
+def search_courses_duck(course_name, platform):
+    load_dotenv()
+    llm = ChatGroq(model="llama3-70b-8192")
+    search = DuckDuckGoSearchRun()
+    agent_with_extractor = create_react_agent(llm, [search])
+    query_2 = f"Найди курсы по {course_name} на платформе {platform}. К каждому из курсов прикрепи ссылку на сам курс."
+    response = agent_with_extractor.invoke(
+        {"messages": [{"role": "user", "content": query_2}]},
+        stream_mode="values"
+    )
+    return response['messages'][-1].content
+    
+
+def get_course_info(course_name: str, platform: str) -> str:
+    load_dotenv()
+    llm = ChatGroq(model="llama3-70b-8192")
     search = DuckDuckGoSearchRun()
     agent_with_extractor = create_react_agent(
         llm,
         [search]
     )
-    query_2 = f"Найди информацию о курсе '{course_name}' на платформе {platform}. Верни ссылку на курс, описание и ключевые темы."
-
-    result_2 = agent_with_extractor.invoke({
-        "messages": [{"role": "user", "content": query_2}]
+    query = f"Найди информацию о курсе '{course_name}' на платформе {platform}. Верни ссылку на курс, описание и ключевые темы."
+    response = agent_with_extractor.invoke({
+        "messages": [{"role": "user", "content": query}]
     })
-
-    response_text_2 = result_2["messages"][-1].content
-
-    return response_text_2
+    return response["messages"][-1].content
 
 
-def get_course_info(course_name, platform):
+def get_course_plan(course_name: str, platform: str) -> str:
+    pass
+
 #    try:
 #
 #        query_3 = f"Проанализируй следующую информацию о курсе '{course_name}' на Stepik:\n{response_text_2}\n\nПредложи учебный план, учитывая следующие параметры: продолжительность курса, сложность, ключевые темы и практические задания. Предложи расписание занятий и советы для эффективного обучения."
@@ -55,7 +101,9 @@ def get_course_info(course_name, platform):
 #    except Exception as e:
 #        print(f"Произошла непредвиденная ошибка: {e}")
 #    return result["messages"][-1].content
-    return 'not implemented'
+
+
+
 
 if __name__ == '__main__':
-    print(search_courses('python programmer', 'stepik'))
+    print(search_courses_duck('python programmer', 'stepik'))
